00d2f7af44ec165546d319ed0dca425d78797a69
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-08 20:31:02 +0300

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,7 @@ export default (await import("astro/config")).defineConfig({
 	// site: "",
 	compressHTML: true,
 	integrations: [
+		// @ts-ignore
 		import.meta.env.MODE === "production"
 			? (await import("astrojs-service-worker")).default()
 			: null,
